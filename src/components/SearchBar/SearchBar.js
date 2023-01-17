d20699740e2cb1f99773e5d25ced73e85982c30f
Tolga SARIKAYA <tolga.sari.kaya@hotmail.com>
2023-01-17 13:29:08 +0300

    input name fixed

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -20,7 +20,9 @@ const SearchBar = (props) => {
         type="text"
         placeholder="Search for a pokemon"
         className={`border-2 border-slate-100 rounded-lg pl-8 py-1 focus:outline-none focus:shadow-md focus:border-slate-300 w-full md:w-full`}
-        onChange={(event) => inputPokemonHandler(event.target.value)}
+        onChange={(event) =>
+          inputPokemonHandler(event.target.value.toLocaleLowerCase())
+        }
       />
     </label>
   );
